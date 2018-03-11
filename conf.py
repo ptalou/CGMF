2901409f1857c17035dbb6a5d9ccb5dd08f000fa
ptalou <ptalou@users.noreply.github.com>
2018-03-11 14:19:57 -0600

    Update conf.py

--- conf.py
+++ conf.py
@@ -32,9 +32,10 @@
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-							'sphinx.ext.todo',
-							'sphinx.ext.autodoc',
-							'sphinx.ext.mathjax']
+	'nbsphinx',
+	'sphinx.ext.todo',
+	'sphinx.ext.autodoc',
+	'sphinx.ext.mathjax']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
