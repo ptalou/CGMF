10cbd64a406c8482a21e9ff9ae109b0a792da5e8
Patrick Talou <talou@lanl.gov>
2018-03-11 14:29:37 -0600

    Updated conf.py and requirements.txt to render ipython notebooks properly.

--- conf.py
+++ conf.py
@@ -32,11 +32,11 @@
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-							'sphinx.ext.todo',
-							'sphinx.ext.autodoc',
-							'sphinx.ext.mathjax',
-							'nbsphinx',
-							'IPython.sphinxext.ipython_console_highlighting']
+	'nbsphinx',
+	'sphinx.ext.todo',
+	'sphinx.ext.autodoc',
+	'sphinx.ext.mathjax',
+	'IPython.sphinxext.ipython_console_highlighting']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
